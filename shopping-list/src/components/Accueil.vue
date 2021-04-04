beb40452d7e11ea5398ff477a13d4028e63c2783
Philippe.boudet <philippe.boudet42@gmail.com>
2021-04-04 11:37:18 +0200

    vue cli commit

---
+++ shopping-list/src/components/Accueil.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="entete">
+      <h1 class="accueil">Shopping list</h1>
+      <p class="layus">petite app pour creer votre liste de courses</p>
+    </div>
+    <div class="corpus">
+      <div class="create">
+        <b-button class="butt" variant="primary">Creer une liste</b-button>
+      </div>
+      <div class="look">
+        <b-button class="butt" variant="primary">Voir la liste</b-button>
+      </div>
+      <div class="supp">
+        <b-button class="butt" variant="primary">supprimer la liste</b-button>
+      </div>
+      <div class="panierimg">
+         <b-img src="../assets/panier.png"></b-img>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Accueil",
+};
+</script>
+<style>
+.accueil {
+  background-color: aquamarine;
+  text-align: center;
+}
+.layus {
+  text-align: center;
+}
+.corpus{
+    text-align: center;
+}
+.butt {
+  min-width: 150px;
+  margin-left: 10px;
+  margin-top: 20px;
+}
+.panierImg{
+
+}
+</style>
